030b3ea77335fbc15ff3749cc8cd1767ad14e7c4
suman-samanta <sumansamanta110@gmail.com>
2024-01-04 01:01:43 +0530

    polling api

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const connectWithDb = require('./config/db');
+require('dotenv').config();
+const app = express();
+const { PORT } = process.env;
+const mongoose = require('mongoose');
+
+// for swagger documentation
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// connect with database
+// connectWithDb();
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  })
+.then(()=>console.log("connection successful...Mongodb Database connected successfully"))
+.catch((err)=>console.log(err));
+
+// regular middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(PORT || 5000, (err) => {
+  if (err) {
+    console.log(`Error in running the server : ${err}`);
+  }
+  console.log(`Server is up and running at ${PORT}`);
+});
